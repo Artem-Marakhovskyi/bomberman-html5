a3f08c363acb4efd53b36ca8f1330423e8848752
Artem Marakhovs'kyi <amara@softserveinc.com>
2018-04-05 11:32:50 +0300

    remove commented

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,14 +2,6 @@ function Game() {
 
     var prepareField = function() {
         
-
-        
-        // for (var i = 0; i < 8; i += 2) {
-        //     for (var j = 0; j < 8; j += 2) {
-        //         ctx.clearRect(20 + i * 32, 20 + j * 32, 32, 32);
-        //         ctx.clearRect(20 + (i + 1) * 32, 20 + (j + 1) * 32, 32, 32);
-        //     }
-        // }
     }
 
     this.start = function() {
